acabe27d8d3b53af73ae7ab0bcaa5f6cfe51935d
VENKATA PRAVEEN KUMAR BELLA <venkata.praveen.kumar.bella@ibm.com>
2025-07-03 22:13:34 +0530

    initial commit

---
+++ Spring_REST_API-department-store-backend-main/src/main/java/com/crud/sql/dto/Cash.java
@@ -0,0 +1,104 @@
+package com.crud.sql.dto;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="Cash")
+public class Cash {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name="floor")
+	private int floor;
+	
+	@OneToMany
+	@JoinColumn(name="id")
+	private List<Sale> sale;
+
+	/**
+	 * void constructor
+	 */
+	public Cash() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param floor
+	 * @param sale
+	 */
+	public Cash(int id, int floor, List<Sale> sale) {
+		super();
+		this.id = id;
+		this.floor = floor;
+		this.sale = sale;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the floor
+	 */
+	public int getFloor() {
+		return floor;
+	}
+
+	/**
+	 * @param floor the floor to set
+	 */
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+	/**
+	 * @return the sale
+	 */
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Sale")
+	public List<Sale> getSale() {
+		return sale;
+	}
+
+	/**
+	 * @param sale the sale to set
+	 */
+	public void setSale(List<Sale> sale) {
+		this.sale = sale;
+	}
+
+	@Override
+	public String toString() {
+		return "Cash [id=" + id + ", floor=" + floor + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ Spring_REST_API-department-store-backend-main/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO cash(id, floor) VALUES (99, 50),(2,56);
+
+INSERT INTO Cashier(id, surname_name) VALUES (99, 'Bella'), (2, 'Greta');
+
+INSERT INTO product(id, name, price) VALUES (99, 'Chocolate', 78),(2,'biscuit',76), (3,'coke',98),(4,'coke',98);
+
+INSERT INTO Sales(id, registered_at, cash_id, cashier_id, product_id) VALUES (99, '2025-07-02', 99, 99, 99), (2,'2025-07-03',2,2,2);
